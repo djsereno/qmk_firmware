11c2910aace16cdf303152d35e3a2fd44a77bce3
djsereno <djsereno91@gmail.com>
2024-03-17 18:04:12 -0700

    Added system volume control

--- keyboards/handwired/dactyl_manuform/5x6_djsereno_digitaljoy/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_djsereno_digitaljoy/keymaps/default/keymap.c
@@ -9,7 +9,8 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_QWERTY] = LAYOUT_5x6(
-        KC_ESC , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,
+        // KC_ESC , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,
+        KC_ESC , KC_F1 , KC_F2 , KC_VOLD , KC_VOLU , KC_F5 ,
         KC_F6  , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,
         KC_F7  , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,
         KC_F8  , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,
