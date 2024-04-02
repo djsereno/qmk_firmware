1603ae71ad74c3350d6ffd63e4c7559ff6e98a31
djsereno <djsereno91@gmail.com>
2024-04-02 00:14:34 -0700

    Add clone hero guitar

---
+++ keyboards/handwired/clonehero_guitar/3x3_djsereno/keymaps/default/config.h
@@ -0,0 +1,30 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define MASTER_LEFT
+
+#define BOOTMAGIC_LITE_ROW 2
+#define BOOTMAGIC_LITE_COLUMN 1
+
+// Min 0, max 32
+// #define JOYSTICK_BUTTON_COUNT 1
+// Min 0, max 6: X, Y, Z, Rx, Ry, Rz
+// #define JOYSTICK_AXIS_COUNT 2
+// Min 8, max 16
+// #define JOYSTICK_AXIS_RESOLUTION 10
---
+++ keyboards/handwired/clonehero_guitar/3x3_djsereno/keymaps/default/keymap.c
@@ -0,0 +1,16 @@
+#include QMK_KEYBOARD_H
+
+#define _QWERTY 0
+
+// joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
+//   JOYSTICK_AXIS_IN(F6, 0, 512, 1023),
+//   JOYSTICK_AXIS_IN(F5, 0, 512, 1023)
+// };
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+        [_QWERTY] = LAYOUT_3x3(
+        KC_A,       KC_S,   KC_J ,
+        KC_K,       KC_L,   KC_UP,
+        KC_ENTER,   KC_H,   KC_DOWN
+    )
+};
